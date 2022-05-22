b5ee04d53412923039311886782c71bd9785950d
zhengrongg <zhengrong29@gmail.com>
2022-05-22 16:45:07 +0800

    Player movements

--- GameDev.tv-2022-Game-Jam/Assets/Scripts/PlayerMovement.cs
+++ GameDev.tv-2022-Game-Jam/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float movementSpeed;
 	// public SpriteRenderer sr;
+	[SerializeField]
+	public GameObject GO_human, GO_ghost;
 
+	void Awake()
+	{
+		//this.GetComponent<PlayerStates>().CurrentPlayerState;
+	}
 	// Start is called before the first frame update
 	//void Start()
 	//{
@@ -17,28 +23,42 @@ public class PlayerMovement : MonoBehaviour
 	void Update()
 	{
 		Vector2 movement = Vector2.zero;
-		if (Input.GetAxis("Vertical") > 0)
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
 		{
 			movement += Vector2.up;
 		}
-		if (Input.GetAxis("Vertical") < 0)
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 		{
 			movement += Vector2.down;
 		}
-		if (Input.GetAxis("Horizontal") < 0)
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
 			movement += Vector2.left;
 		}
-		if (Input.GetAxis("Horizontal") > 0)
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
 			movement += Vector2.right;
+			//sr.flipX = !sr.flipX;
+
 		}
 
 		if (movement != Vector2.zero)
 		{
 			movement.Normalize();
 			movement = movementSpeed * Time.deltaTime * movement;
-			transform.Translate(movement, Space.World);
+			if (this.GetComponent<PlayerStates>().CurrentPlayerState == PlayerStates.PlayerExistance.Human)
+			{
+				GO_human.transform.Translate(movement, Space.World);
+			}
+			if (this.GetComponent<PlayerStates>().CurrentPlayerState == PlayerStates.PlayerExistance.Ghost)
+			{
+				GO_ghost.transform.Translate(movement, Space.World);
+			}
+		}
+
+		if (this.GetComponent<PlayerStates>().CurrentPlayerState == PlayerStates.PlayerExistance.Human)
+		{
+			GO_ghost.transform.position = GO_human.transform.position;
 		}
 	}
 }
