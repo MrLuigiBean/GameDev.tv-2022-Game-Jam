9ba1fc3b5b3fbc2e577e13564d1a8262cdd184c8
zhengrongg <zhengrong29@gmail.com>
2022-05-25 00:20:05 +0800

    i did X camera clamping. but it breaks once Y gets in. ill be trying another method

--- GameDev.tv-2022-Game-Jam/Assets/Scripts/CameraTrailing.cs
+++ GameDev.tv-2022-Game-Jam/Assets/Scripts/CameraTrailing.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class CameraTrailing : MonoBehaviour
 {
-	public Transform ghost_trfm, leftBound_trfm, rightBound_trfm;
+	public Transform ghost_trfm, leftBound_trfm, rightBound_trfm, topBound_trfm, bottomBound_trfm;
 	public float smoothingSpeed;
-	public Vector3 offset;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -16,18 +15,23 @@ public class CameraTrailing : MonoBehaviour
 	// Update is called once per frame
 	void LateUpdate()
 	{
-		Vector3 leftPos = this.GetComponent<Camera>().WorldToViewportPoint(leftBound_trfm.position);
+		Vector3 leftBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(leftBound_trfm.position);
+		Vector3 rightBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(rightBound_trfm.position);
+		Vector3 topBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(topBound_trfm.position);
+		Vector3 bottomBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(bottomBound_trfm.position);
 		Vector3 targetPos = this.GetComponent<Camera>().WorldToViewportPoint(ghost_trfm.position);
-		Vector3 targetPosition = ghost_trfm.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, targetPosition, smoothingSpeed);
-		//this.transform.position = smoothedPosition;
-		
-		//Bounds checking
-		//Left bounds out of camera frame
-		if (leftPos.x <= 0 || targetPos.x >= 0.65)
-		{
-			this.transform.position = new Vector3(smoothedPosition.x, 0, smoothedPosition.z);
-		}
+		Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, ghost_trfm.position, smoothingSpeed);
 
+		//Bounds checking
+		//X
+		if ((leftBoundPos.x <= 0 || targetPos.x >= 0.55) && (rightBoundPos.x >= 1 || targetPos.x <= 0.45))
+		{
+			this.transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, -10);
+		}
+		//Y - I CANT SEEM TO DO Y
+		//if ((bottomBoundPos.y <= 0 || targetPos.y >= 0.55) && (topBoundPos.y >= 1 || targetPos.y <= 0.45))
+		//{
+		//	this.transform.position = new Vector3(0, smoothedPosition.y, -10);
+		//}
 	}
 }
