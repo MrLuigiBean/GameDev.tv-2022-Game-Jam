ff7e38a3c414943ea311a9781614a29f08fc759f
zhengrongg <zhengrong29@gmail.com>
2022-05-25 01:26:28 +0800

    Camera done

--- GameDev.tv-2022-Game-Jam/Assets/Scripts/CameraTrailing.cs
+++ GameDev.tv-2022-Game-Jam/Assets/Scripts/CameraTrailing.cs
@@ -9,29 +9,22 @@ public class CameraTrailing : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		//this.transform.position = ghost_trfm.position;
 	}
 
 	// Update is called once per frame
 	void LateUpdate()
 	{
-		Vector3 leftBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(leftBound_trfm.position);
-		Vector3 rightBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(rightBound_trfm.position);
-		Vector3 topBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(topBound_trfm.position);
-		Vector3 bottomBoundPos = this.GetComponent<Camera>().WorldToViewportPoint(bottomBound_trfm.position);
-		Vector3 targetPos = this.GetComponent<Camera>().WorldToViewportPoint(ghost_trfm.position);
-		Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, ghost_trfm.position, smoothingSpeed);
+		float aspectRatio = this.GetComponent<Camera>().aspect;
+		float orthographicSize = this.GetComponent<Camera>().orthographicSize;
+		float leftBoundOffset = leftBound_trfm.position.x + (orthographicSize * aspectRatio);
+		float rightBoundOffset = rightBound_trfm.position.x - (orthographicSize * aspectRatio);
+		float topBoundOffset = topBound_trfm.position.y - orthographicSize;
+		float bottomBoundOffset = bottomBound_trfm.position.y + orthographicSize;
+		Vector3 boundPosition = new Vector3(Mathf.Clamp(ghost_trfm.localPosition.x, leftBoundOffset, rightBoundOffset),
+											Mathf.Clamp(ghost_trfm.localPosition.y, bottomBoundOffset, topBoundOffset),
+											-10);
 
-		//Bounds checking
-		//X
-		if ((leftBoundPos.x <= 0 || targetPos.x >= 0.55) && (rightBoundPos.x >= 1 || targetPos.x <= 0.45))
-		{
-			this.transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, -10);
-		}
-		//Y - I CANT SEEM TO DO Y
-		//if ((bottomBoundPos.y <= 0 || targetPos.y >= 0.55) && (topBoundPos.y >= 1 || targetPos.y <= 0.45))
-		//{
-		//	this.transform.position = new Vector3(0, smoothedPosition.y, -10);
-		//}
+		Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, boundPosition, smoothingSpeed);
+		this.transform.position = smoothedPosition;
 	}
 }
